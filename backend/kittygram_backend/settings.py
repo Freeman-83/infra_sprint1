71079ffc0dabc1ba5bb4bb8e3ba48e03ec459e39
Михаил Марин <mmn-83@yandex.ru>
2023-08-04 15:20:31 +0000

    rev 1.1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY: str = os.getenv('SECRET_KEY')
-DEBUG = False
+DEBUG: bool = os.getenv('DEBUG') == True
 
 ALLOWED_HOSTS = ['51.250.101.169', '127.0.0.1', 'localhost', 'catownersclub.ddns.net']
 
